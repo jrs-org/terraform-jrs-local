353187335ce52ea69666a880aed6a0a3b298d59d
Josue-RS <josue.ruiz@wizeline.com>
2024-01-10 13:11:02 -0600

    FIX: change order creation repos resources

--- modules/repos/main.tf
+++ modules/repos/main.tf
@@ -37,59 +37,6 @@ resource "github_branch_default" "default" {
   depends_on = [github_repository.repository_branch_autoninit]
 }
 
-#branch protection
-# Protect the main branch of the foo repository. Additionally, require that
-# the "ci/check" check ran by the Github Actions app is passing and only allow
-# the engineers team merge to the branch.
-
-resource "github_branch_protection" "branch_policy" {
-  for_each      = { for repo in var.repositories : repo.name => repo }
-  repository_id = each.value.name
-  pattern       = each.value.default_branch
-  depends_on    = [github_branch_default.default]
-
-  enforce_admins   = true
-  allows_deletions = false
-
-  required_status_checks {
-    strict   = true
-    contexts = ["ci/github-pipelines.yml"]
-  }
-
-  required_pull_request_reviews {
-    dismiss_stale_reviews           = true
-    restrict_dismissals             = false
-    require_code_owner_reviews      = true
-    required_approving_review_count = 2
-    # dismissal_restrictions = [
-    #   data.github_user.example.node_id,
-    #   github_team.example.node_id,
-    #   "/exampleuser",
-    #   "exampleorganization/exampleteam",
-    # ]
-  }
-
-  #   push_restrictions = [
-  #     # data.github_user.example.node_id,
-  #     "/exampleuser",
-  #     # "exampleorganization/exampleteam",
-  #     # you can have more than one type of restriction (teams + users). If you use
-  #     # more than one type, you must use node_ids of each user and each team.
-  #     # github_team.example.node_id
-  #     # github_user.example-2.node_id
-  #   ]
-
-  #   force_push_bypassers = [
-  #     # data.github_user.example.node_id,
-  #     "/exampleuser",
-  #     # "exampleorganization/exampleteam",
-  #     # you can have more than one type of restriction (teams + users)
-  #     # github_team.example.node_id
-  #     # github_team.example-2.node_id
-  #   ]
-
-}
-
 # add pipeline template to all repos in tfvars
 resource "github_repository_file" "pipeline_file" {
   count               = terraform.workspace != "dev" ? 0 : length(var.repositories)
@@ -99,6 +46,7 @@ resource "github_repository_file" "pipeline_file" {
   content             = file("${path.cwd}/assets/github-pipelines.yml") #,{ type = var.repositories[count.index].type })
   commit_message      = "Añade o actualiza pipelines_file via terraform ***NO_CI***"
   overwrite_on_create = false
+  depends_on = [ github_branch_default.default ]
 
   lifecycle {
     ignore_changes = [
@@ -117,6 +65,7 @@ resource "github_repository_file" "pr_template" {
   content             = file("${path.cwd}/assets/pull_request_template.md") #,{ type = var.repositories[count.index].type })
   commit_message      = "Añade o actualiza pr_template via terraform ***NO_CI***"
   overwrite_on_create = false
+  depends_on = [ github_repository_file.pipeline_file ]
 
   lifecycle {
     ignore_changes = [
@@ -159,4 +108,57 @@ resource "github_repository_file" "tf_apply_template" {
       commit_message
     ]
   }
+}
+
+#branch protection
+# Protect the main branch of the foo repository. Additionally, require that
+# the "ci/check" check ran by the Github Actions app is passing and only allow
+# the engineers team merge to the branch.
+
+resource "github_branch_protection" "branch_policy" {
+  for_each      = { for repo in var.repositories : repo.name => repo }
+  repository_id = each.value.name
+  pattern       = each.value.default_branch
+  depends_on    = [github_repository_file.pr_template]
+
+  enforce_admins   = true
+  allows_deletions = false
+
+  required_status_checks {
+    strict   = true
+    contexts = ["ci/github-pipelines.yml"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    restrict_dismissals             = false
+    require_code_owner_reviews      = true
+    required_approving_review_count = 2
+    # dismissal_restrictions = [
+    #   data.github_user.example.node_id,
+    #   github_team.example.node_id,
+    #   "/exampleuser",
+    #   "exampleorganization/exampleteam",
+    # ]
+  }
+
+  #   push_restrictions = [
+  #     # data.github_user.example.node_id,
+  #     "/exampleuser",
+  #     # "exampleorganization/exampleteam",
+  #     # you can have more than one type of restriction (teams + users). If you use
+  #     # more than one type, you must use node_ids of each user and each team.
+  #     # github_team.example.node_id
+  #     # github_user.example-2.node_id
+  #   ]
+
+  #   force_push_bypassers = [
+  #     # data.github_user.example.node_id,
+  #     "/exampleuser",
+  #     # "exampleorganization/exampleteam",
+  #     # you can have more than one type of restriction (teams + users)
+  #     # github_team.example.node_id
+  #     # github_team.example-2.node_id
+  #   ]
+
 }
