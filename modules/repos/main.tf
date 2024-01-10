e203ec3659782f2027f55ba2c6fde7788513f380
Josue-RS <josue.ruiz@wizeline.com>
2024-01-10 13:59:51 -0600

    FIX: remove branch policies

--- modules/repos/main.tf
+++ modules/repos/main.tf
@@ -110,55 +110,55 @@ resource "github_repository_file" "tf_apply_template" {
   }
 }
 
-#branch protection
-# Protect the main branch of the foo repository. Additionally, require that
-# the "ci/check" check ran by the Github Actions app is passing and only allow
-# the engineers team merge to the branch.
+# #branch protection
+# # Protect the main branch of the foo repository. Additionally, require that
+# # the "ci/check" check ran by the Github Actions app is passing and only allow
+# # the engineers team merge to the branch.
 
-resource "github_branch_protection" "branch_policy" {
-  for_each      = { for repo in var.repositories : repo.name => repo }
-  repository_id = each.value.name
-  pattern       = each.value.default_branch
-  depends_on    = [github_repository_file.pr_template]
+# resource "github_branch_protection" "branch_policy" {
+#   for_each      = { for repo in var.repositories : repo.name => repo }
+#   repository_id = each.value.name
+#   pattern       = each.value.default_branch
+#   depends_on    = [github_repository_file.pr_template]
 
-  enforce_admins   = true
-  allows_deletions = false
+#   enforce_admins   = true
+#   allows_deletions = false
 
-  required_status_checks {
-    strict   = true
-    contexts = ["ci/github-pipelines.yml"]
-  }
+#   required_status_checks {
+#     strict   = true
+#     contexts = ["ci/github-pipelines.yml"]
+#   }
 
-  required_pull_request_reviews {
-    dismiss_stale_reviews           = true
-    restrict_dismissals             = false
-    require_code_owner_reviews      = true
-    required_approving_review_count = 2
-    # dismissal_restrictions = [
-    #   data.github_user.example.node_id,
-    #   github_team.example.node_id,
-    #   "/exampleuser",
-    #   "exampleorganization/exampleteam",
-    # ]
-  }
+#   required_pull_request_reviews {
+#     dismiss_stale_reviews           = true
+#     restrict_dismissals             = false
+#     require_code_owner_reviews      = true
+#     required_approving_review_count = 2
+#     # dismissal_restrictions = [
+#     #   data.github_user.example.node_id,
+#     #   github_team.example.node_id,
+#     #   "/exampleuser",
+#     #   "exampleorganization/exampleteam",
+#     # ]
+#   }
 
-  #   push_restrictions = [
-  #     # data.github_user.example.node_id,
-  #     "/exampleuser",
-  #     # "exampleorganization/exampleteam",
-  #     # you can have more than one type of restriction (teams + users). If you use
-  #     # more than one type, you must use node_ids of each user and each team.
-  #     # github_team.example.node_id
-  #     # github_user.example-2.node_id
-  #   ]
+#   #   push_restrictions = [
+#   #     # data.github_user.example.node_id,
+#   #     "/exampleuser",
+#   #     # "exampleorganization/exampleteam",
+#   #     # you can have more than one type of restriction (teams + users). If you use
+#   #     # more than one type, you must use node_ids of each user and each team.
+#   #     # github_team.example.node_id
+#   #     # github_user.example-2.node_id
+#   #   ]
 
-  #   force_push_bypassers = [
-  #     # data.github_user.example.node_id,
-  #     "/exampleuser",
-  #     # "exampleorganization/exampleteam",
-  #     # you can have more than one type of restriction (teams + users)
-  #     # github_team.example.node_id
-  #     # github_team.example-2.node_id
-  #   ]
+#   #   force_push_bypassers = [
+#   #     # data.github_user.example.node_id,
+#   #     "/exampleuser",
+#   #     # "exampleorganization/exampleteam",
+#   #     # you can have more than one type of restriction (teams + users)
+#   #     # github_team.example.node_id
+#   #     # github_team.example-2.node_id
+#   #   ]
 
-}
+# }
