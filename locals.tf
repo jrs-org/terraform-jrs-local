c6ca7d522fea92e68c7127dd45bd720297a35e0d
Josue-RS <josue.ruiz@wizeline.com>
2024-01-10 15:14:52 -0600

    FIX: remove required status check

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  tf_plan_template = [for repo in var.repositories : repo if repo.name == "terraform-jrs-local"]
+  tf_plan_template    = [for repo in var.repositories : repo if repo.name == "terraform-jrs-local"]
   tf_repo_branch_prot = [for repo in var.repositories : repo if repo.name != "terraform-jrs-local"]
 }
