74c5848aa9095e4fb8fb935f9815e649354da653
Josue-RS <josue.ruiz@wizeline.com>
2024-01-15 16:23:25 -0600

    FIX: delete variables for plan out

--- modules/repos/variables.tf
+++ modules/repos/variables.tf
@@ -8,6 +8,10 @@ variable "repositories" {
   }))
 
   description = "List of repositories"
+  # validation {
+  #   condition     = length([for repo in var.repositories : can(regex("^jrs-(store|central)-.*$", repo.name)) || repo.name == "terraform-jrs-local" if !can(regex("^jrs-(store|central)-.*$", repo.name)) && repo.name != "terraform-jrs-local"]) == 0
+  #   error_message = "Each repository name, except 'terraform-jrs-local', must start with 'jrs-' followed by either 'store' or 'central', then '-', and then any characters.\n"
+  # }
 }
 
 # variable "repository_name" {
