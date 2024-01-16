681b5bd0b3616a0a760f4e5c87ca5f03ebe5ab6c
Josue-RS <josue.ruiz@wizeline.com>
2024-01-16 12:36:10 -0600

    FIX: change to private repos

--- modules/repos/main.tf
+++ modules/repos/main.tf
@@ -22,7 +22,7 @@ resource "github_repository" "repository_branch_autoinit" {
   # Configuration options
   name        = each.value.name
   description = "Repository managed by Terraform"
-  visibility  = "internal"
+  visibility  = "private"
   #   default_branch = each.value.default_branch
   auto_init = true
 
